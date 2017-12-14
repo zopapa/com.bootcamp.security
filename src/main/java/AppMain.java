5c3630490ef742563a834307c52646c03be9ada6
fbr <bignonfebron@gmail.com>
2017-12-14 19:50:46 +0100

    just to commit

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -3,14 +3,14 @@ import com.bootcamp.security.TokenManager;
 
 public class AppMain {
     public static void main(String[] args) {
-        User user = new User();
+    /*    User user = new User();
         user.setLogin("fefe");
         user.setPassword("fefep");
 
         String token =TokenManager.generate(user);
         System.out.println("Voici le token "+token+"\n");
         boolean b = TokenManager.verifyToken(token);
-        System.out.println("Is good :::: "+b+"\n");
+        System.out.println("Is good :::: "+b+"\n");  */
     }
 
 }
